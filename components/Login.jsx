a6a8adff6791eb1193c9be7070991ab18825bd64
Higor Pires <higorlpires@gmail.com>
2022-08-11 10:07:54 -0300

    commit

---
+++ components/Login.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+const Login = () => {
+
+
+
+
+  return (
+  <div className = 'login'>
+      <div className = 'position'>
+        <div className = 'loginBox'>
+            <form action ='/api/register' method = 'POST'>
+                <input placeholder = 'Login' type= 'email' name = 'email'></input>
+                <input placeholder = 'Senha' type= 'password' name = 'password'></input>
+                {/*<input placeholder = 'CPF'></input>*/}
+                
+                
+                <button type ='submit' value = 'Register'></button>
+            </form>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Login
