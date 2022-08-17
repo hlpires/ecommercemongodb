701de5a845ac8c79f81cd4678b24e5c7bc836f97
hlpires <higorlpires@gmail.com>
2022-08-17 18:35:53 -0300

    advance in slug

--- pages/produtos/[slug].js
+++ pages/produtos/[slug].js
@@ -1,12 +1,23 @@
-import React from 'react'
+import React,{useState} from 'react'
 import {useRouter} from 'next/router'
 import Header from '../../components/Header'
+import Produtos from '../../components/Produtos'
 
 const produtosSlug = () => {
   
 
   const router =  useRouter()
   const {query:{name,imageurl,preco}} = router
+  const [numero,setNumero] = useState(1)
+
+  const incNum = () => {
+    setNumero((prevQty) => prevQty + 1);
+  }
+
+  const decNum = () => { 
+    setNumero((prevQty) => prevQty - 1);
+  }
+  
 
 
   return (
@@ -21,11 +32,22 @@ const produtosSlug = () => {
              <p className ='tittleSlug'>{name}</p>
              <img className ='image' src ={imageurl}></img>
              </div>
-           <div className ='details'></div>
+           <div className ='details'>
+           <div className ='detailsBox'>
+           <p className ='tittleSlug'>{name}</p>
+           <p className ='priceSlug'><span className = 'real'>R$</span>{preco+',00'}</p>
+           <div className ='quantidadeBox'>
+             <div className ='counter' id ='counter1' onClick = {decNum}></div>
+             <div className = 'numero'>{numero}</div>
+             <div className ='counter'  id ='counter2' onClick = {incNum}></div>
+           </div>
+           </div>
+           </div>
 
          </div>
        </div>
-      </div> 
+      </div>
+      <Produtos/>
     </div>
   )
 }
