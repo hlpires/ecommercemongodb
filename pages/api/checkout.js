2e98c549f33c6e85ed5b7c17470313b78160cf25
hlpires <higorlpires@gmail.com>
2022-09-19 08:47:19 -0300

    Update checkout.js

--- pages/api/checkout.js
+++ pages/api/checkout.js
@@ -17,7 +17,6 @@ export default async function handler(req,res) {
             mode: 'payment',
             payment_method_types: ['card'],
             billing_address_collection: 'auto',
-
             line_items: req.body.map((item)=> {
 
               
